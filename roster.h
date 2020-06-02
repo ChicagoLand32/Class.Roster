0c601cda23223a5d93a4b3de2eae55137dfd7c23
JoshHarrison1212 <66268360+JoshHarrison1212@users.noreply.github.com>
2020-06-01 21:11:05 -0600

    Update roster.h

--- roster.h
+++ roster.h
@@ -26,7 +26,7 @@ string studentData[numStudents] = {
 	"A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK",
 	"A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE",
 	"A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY",
-	"A5, Josh,Harrison,jhar954@wgu.edu,27,30,37,32, SOFTWARE"
+	"A5,
 };
 class Roster {
 public:
